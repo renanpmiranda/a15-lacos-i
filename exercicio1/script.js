7b26ba1b8d1343b605509e05df571b2545d1cc27
renanpmiranda <renanpmiranda5@gmail.com>
2022-09-12 13:19:08 -0300

    a015_exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+let coxinha = prompt('Deseja comer mais coxinhas?')
+let conta = 0
+
+while(coxinha === 'S'){
+    conta += 2.50
+    coxinha = prompt('Deseja comer mais coxinhas?')
+}
+
+if (coxinha === 'N'){
+    console.log(`Nª de coxinhas consumidas: ${conta/2.50}\nValor da conta: R$${conta}`)
+}
